6f763dd0a5139e9c5064882466b3853e2578b5f9
enzo crivelli <enzo_crivelli97@hotmail.com>
2020-09-26 19:56:58 -0300

    new project

---
+++ app/views/orden_trabajos/_orden_trabajo.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! orden_trabajo, :id, :trnum, :trcan, :trcar, :clinom, :papel, :gramaje, :colores, :pliego, :nomprod, :fecentr, :cam10, :cam12, :cam24, :created_at, :updated_at
+json.url orden_trabajo_url(orden_trabajo, format: :json)
---
+++ db/migrate/20200926224822_create_orden_trabajos.rb
@@ -0,0 +1,21 @@
+class CreateOrdenTrabajos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orden_trabajos do |t|
+      t.integer :trnum
+      t.integer :trcan
+      t.date :trcar
+      t.string :clinom
+      t.string :papel
+      t.integer :gramaje
+      t.string :colores
+      t.integer :pliego
+      t.string :nomprod
+      t.date :fecentr
+      t.string :cam10
+      t.string :cam12
+      t.string :cam24
+
+      t.timestamps
+    end
+  end
+end
