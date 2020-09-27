046cf2ed902a0a4f09747d47bf7bfd80ec131f89
enzo crivelli <enzo_crivelli97@hotmail.com>
2020-09-27 19:32:50 -0300

    rearmando de vuelta

--- app/controllers/orden_trabajos_controller.rb
+++ app/controllers/orden_trabajos_controller.rb
@@ -1,10 +1,42 @@
 class OrdenTrabajosController < ApplicationController
+
   before_action :set_orden_trabajo, only: [:show, :edit, :update, :destroy]
+  before_action :listado_trabajo, only:[:digital, :offset, :post1, :post2, :post3, :post4, :post5, :post6, :post7]
 
   # GET /orden_trabajos
   # GET /orden_trabajos.json
-  def index
-    @orden_trabajos = OrdenTrabajo.all
+def index
+    @orden_trabajos = OrdenTrabajo.all.order('clinom ASC')
+    respond_to do |format|
+      format.html # index.html.erb
+      format.js # index.js.erb
+      format.json { render json: @orden_trabajos}
+             format.pdf do
+        render pdf: 'listado/pdf', pdf: 'Listado'
+      end
+    end
+  end
+
+
+  def listado
+    @orden_trabajos = OrdenTrabajo.all.order('clinom ASC')
+    respond_to do |format|
+      format.html # index.html.erb
+      format.js # index.js.erb
+      format.json { render json: @orden_trabajos}
+
+      format.xlsx {
+        response.headers[
+          'Content-Disposition'
+        ] = "attachment; filename = Listado_ordenes_trabajo.xlsx"
+      }
+
+      format.pdf do
+        render pdf: 'listado/pdf', pdf: 'Listado',
+        :orientation => 'landscape'
+      end
+
+    end
   end
 
   # GET /orden_trabajos/1
@@ -61,14 +93,44 @@ class OrdenTrabajosController < ApplicationController
     end
   end
 
+
+# ESTE SECTOR DEL CONTROLADOR ES PARA LAS DIFERENTES VIEWS DE IMPRESIÓN Y POST
+# ------------------------------------------------------------------------
+def digital
+end
+def offset
+end
+def post1
+end
+def post2
+end
+def post3
+end
+def post4
+end
+def post5
+end
+def post6
+end
+def post7
+end
+
+# ------------------------------------------------------------------------
+
   private
     # Use callbacks to share common setup or constraints between actions.
+
+    def listado_trabajo
+      @orden_trabajos = OrdenTrabajo.all.order('fecentr ASC')
+    end
+
+
     def set_orden_trabajo
       @orden_trabajo = OrdenTrabajo.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def orden_trabajo_params
-      params.require(:orden_trabajo).permit(:trnum, :trcan, :trcar, :clinom, :papel, :gramaje, :colores, :pliego, :nomprod, :fecentr, :cam10, :cam12, :cam24)
+      params.require(:orden_trabajo).permit(:trnum, :clinom, :nomprod,  :fecentr, :procesos, :observaciones, :estado_actual, :estado)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_224822) do
+ActiveRecord::Schema.define(version: 2020_09_27_200403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2020_09_26_224822) do
     t.string "cam24"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "observaciones"
+    t.string "procesos"
+    t.string "estado_actual"
   end
 
 end
