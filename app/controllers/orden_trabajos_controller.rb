8335d9c4d0541f47b197a630a2f642d98d0270c7
enzo crivelli <enzo_crivelli97@hotmail.com>
2020-10-05 23:58:23 -0300

    add stimulus to submit button and ajax to delete option

--- app/controllers/orden_trabajos_controller.rb
+++ app/controllers/orden_trabajos_controller.rb
@@ -36,7 +36,7 @@ class OrdenTrabajosController < ApplicationController
         render pdf: 'listado/pdf', pdf: 'Listado',
         :orientation => 'landscape'
       end
-    end  
+    end
   end
 
 
@@ -95,7 +95,7 @@ class OrdenTrabajosController < ApplicationController
   def update
     respond_to do |format|
       if @orden_trabajo.update(orden_trabajo_params)
-        format.html { redirect_to action: "index", notice: 'Orden trabajo was successfully updated.' }
+        format.html
         format.json { render :show, status: :ok, location: @orden_trabajo }
       else
         format.html { render :edit }
@@ -109,8 +109,9 @@ class OrdenTrabajosController < ApplicationController
   def destroy
     @orden_trabajo.destroy
     respond_to do |format|
-      format.html { redirect_to orden_trabajos_url, notice: 'Orden trabajo was successfully destroyed.' }
+      format.html
       format.json { head :no_content }
+      format.js { render :layout => false }
     end
   end
 
