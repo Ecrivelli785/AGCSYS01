5df2b2b701a488c1710e15cb0998441bd5daa5d5
enzo crivelli <enzo_crivelli97@hotmail.com>
2020-10-13 00:16:27 -0300

    change hash to array

--- app/models/orden_trabajo.rb
+++ app/models/orden_trabajo.rb
@@ -1,3 +1,5 @@
 class OrdenTrabajo < ApplicationRecord
-  POST = { "1" => "SM", "2" => "PM", "3" => "GTO", "4" => "SORZ", "5" => "Ko", "6" => "Xe", "7" => "Buv", "8" => "Lam", "9" => "Tipografia", "10" => "Fidia", "11" => "Baum","12" => "Duplo", "13" =>"C.Hilo", "14" => "Binder", "15" => "Zechini", "16" => "Alzadora", "17" => "C.Alambre", "18" => "T.Manual", "19" => "Otros", "20" =>"Renz", "21" => "Post"}
+
+POST =  ["SM","PM","GTO", "SORZ", "Ko", "Xe","Buv","Lam","Tipografia","Fidia","Baum","Duplo", "C. Hilo", "Binder", "Zechini", "Alzadora","C.Alambre","T.Manual","Otros"," Post" ,"Renz"]
+
 end
